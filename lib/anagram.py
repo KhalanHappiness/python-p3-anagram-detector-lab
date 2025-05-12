1996a94686a95a524b064d63be3d057bec327772
happiness <happinesskhalan@gmail.com>
2025-05-13 00:11:22 +0300

    Anagram detector lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,29 @@
 # your code goes here!
+#class Anagram:
+    
+     #def constructur(self and word)
+         #set self.word = word
+     #def match(self, word_list = list())
+         #
+from collections import Counter
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+        pass
+    def match(self, word_list):
+        anagram_list = list()
+
+        for word_value in word_list:
+            if Counter(word_value) == Counter(self.word):
+                anagram_list.append(word_value)
+            else:
+                anagram_list 
+        return anagram_list
+
+
+
+        pass
+
+listen = Anagram("pay")
+print(listen.match(['enlists', 'google', 'inlets', 'yap']))
